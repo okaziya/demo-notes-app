e4b0fc8ef629317c4990a7fc89546c5f17ff9467
Liza <eolshevskaya@purple-technology.com>
2023-02-28 20:10:07 +0100

    Enabling CORS

--- stacks/StorageStack.js
+++ stacks/StorageStack.js
@@ -3,7 +3,16 @@ import { Table, Bucket } from "@serverless-stack/resources";
 export function StorageStack({ stack, app }) {
 
   // Create an S3 bucket
-  const bucket = new Bucket(stack, "Uploads");
+  const bucket = new Bucket(stack, "Uploads", {
+    cors: [
+      {
+        maxAge: "1 day",
+        allowedOrigins: ["*"],
+        allowedHeaders: ["*"],
+        allowedMethods: ["GET", "PUT", "POST", "DELETE", "HEAD"],
+      },
+    ],
+  });
   // Create the DynamoDB table
 
   // userId: The id of the user that the note belongs to.
